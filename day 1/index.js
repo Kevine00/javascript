7303f83ddf7b59b33dd0dc09a3227ba2f41395de
kevine <kevineumubyeyi9@gmail.com>
2023-11-21 07:15:17 -0800

    operators and variables

---
+++ day 1/index.js
@@ -0,0 +1,48 @@
+//this is how to declare variables
+var a;
+
+//this is how to store value with assignment operator
+var a=20;
+
+//this is how to assign value of one variable to another
+var x;
+x=20
+var y;
+y=x;
+
+//this is how you should declare a string value
+var myName= "umubyeyi";
+var myName= "kevine";
+
+//this is how you should add two numbers
+const sum= 20+15;
+console.log(sum)
+
+//this is how you should multiply numbers
+
+const product = 10*5;
+console.log(product);
+
+//this is how you should divide decimal numbers
+
+const qoutient = 1.3 / 3.4;
+console.log(qoutient);
+
+//this is how you should find a reminder 
+
+const reminder = 10%3;
+console.log(remainder)
+
+//this is how you should do compound assignment with augemented division
+
+let x=12;
+let y=14;
+let z=16;
+
+x /=2;
+y /=7;
+z /=8;
+
+
+
+
