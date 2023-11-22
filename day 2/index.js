970f397d534b830c5c37c3b7e6f3e34a2b398d5c
kevine <kevineumubyeyi9@gmail.com>
2023-11-22 06:45:10 -0800

    boolean

---
+++ day 2/index.js
@@ -0,0 +1,28 @@
+
+
+
+// in boolean we have only two values true and false
+
+console.log(isOnSale);
+isOnSale = false;
+console.log(isOnSale); //here the output will the first one will be true and  the second one will be false 
+
+
+//here is how we can work with other datatype such as numbers in boolean context
+console.log(boolean(100))//output will be true
+
+//here is another example
+const price = 10;
+console.log(Boolean(price)); // output will be true
+
+const name = 'james';
+console.log(Boolean(name)); //output will be true
+
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean(-1)); //output will be true ,false,true
+
+
+
+
+
